f780bc9c63370430b1f0706bcd986cf98f47a053
dimon.obert <dimon.obert@quintagroup.org>
2016-11-28 11:52:45 +0200

    init commit

---
+++ patch.py
@@ -0,0 +1,18 @@
+from jsonpatch import apply_patch as patch
+import sys
+import json
+
+
+with open('OCDS/release-schema.json') as content:
+    standart = json.load(content)
+with open('extension.json') as content:
+    extension = json.load(content)
+
+name = sys.argv[1]
+
+res = patch(standart, extension)
+
+with open('Patched/{}.json'.format(name), 'w') as outfile:
+    json.dump(res, outfile)
+with open('Patches/{}.json'.format(name), 'w') as outfile:
+    json.dump(extension, outfile)
