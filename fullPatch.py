6702d09c784d4a3c5d7fbadddf12733675ca7215
dimon.obert <dimon.obert@quintagroup.org>
2016-11-29 15:23:22 +0200

    updates

--- fullPatch.py
+++ fullPatch.py
@@ -3,15 +3,15 @@ import jsonpatch
 import os
 
 def open_json(path):
-	with open(path) as content:
-		return json.load(content)
+    with open(path) as content:
+        return json.load(content)
 
 OCDS = open_json("OCDS/release-schema.json")
 for filename in os.listdir("Patches"):
-	path = os.path.join("Patches", filename)
-	print path
-	patched = jsonpatch.apply_patch(OCDS, open_json(path))
-	OCDS = patched
+    path = os.path.join("Patches", filename)
+    patched = jsonpatch.apply_patch(OCDS, open_json(path))
+    OCDS = patched
 
 with open('Full/Full_schema.json', 'w') as outfile:
     json.dump(OCDS, outfile, indent=4)
+
